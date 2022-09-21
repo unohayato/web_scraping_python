a36ae5acf4c48dd1f1bb497cc42055996bf7092d
unohayato <mamidori1p1p@gmail.com>
2022-09-21 14:09:27 +0900

    get a h2 tag

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import imp
 import requests
-res = requests.get('https://www.orangepage.net/recipes')
-print(res.text)
+from bs4 import BeautifulSoup
+
+res = requests.get('https://www.orangepage.net/recipes/search/6')
+soup = BeautifulSoup(res.text, 'html.parser')
+h2_tags = soup.find_all('h2')
+print(h2_tags[2])
