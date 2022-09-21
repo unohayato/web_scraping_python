867aa3b97d98bb01218c792555a8de93a5007935
unohayato <mamidori1p1p@gmail.com>
2022-09-21 14:24:35 +0900

    get title only

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import imp
+
 import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://www.orangepage.net/recipes/search/6')
 soup = BeautifulSoup(res.text, 'html.parser')
-h2_tags = soup.find_all('h2')
-print(h2_tags[2])
+
+recipes = soup.find('div', id='recipe_list', class_='recipesList')
+p_tit_tags = recipes.find_all('h2', class_='tit')
+p_tit_str = [x.string for x in p_tit_tags]
+print(p_tit_str)
